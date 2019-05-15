e1f287fc0d3dbfaa782cf23c1281a66099b75354
Micah Stubbs <micah.stubbs@gmail.com>
2019-05-15 01:15:59 -0700

    prefer local data

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,4 @@
-d3.json(
-  "https://gist.githubusercontent.com/bumbeishvili/dc0d47bc95ef359fdc75b63cd65edaf2/raw/c33a3a1ef4ba927e3e92b81600c8c6ada345c64b/orgChart.json"
-).then(data => {
+d3.json("./orgChart.json").then(data => {
   Chart()
     .container(".chart-container")
     .data(data)
